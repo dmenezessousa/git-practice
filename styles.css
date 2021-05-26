2c4770e35ee33fe37625da7e26c1d2b644fcb188
Diego Sousa <dmenezessousaa@gmail.com>
2021-05-26 16:05:36 -0400

    Create styles.css

---
+++ styles.css
@@ -0,0 +1,22 @@
+# **Diego Sousa**
+<a href="https://ibb.co/3rJchWW"><img src="https://i.ibb.co/JBhHk22/Me.jpg" alt="Me" border="0"></a>  
+  
+*Hello, My name is Diego Sousa, I'm a USMC veteran.   
+Currently working as a Fire Alarm Technician and studying Web Development on [Code Immersives](https://www.codeimmersives.com/).*  
+
+---  
+
+## Hobbies
+
+- [Workout](https://lafitness.com/Pages/Default.aspx)
+- [Play Video Games](https://www.twitch.tv/)
+- [Learning Coding](https://www.codeimmersives.com/)
+
+---  
+
+## Work Experience  
+
+| From - To     | Occupation    |
+| ------------- |:-------------:|
+| 2013-2017     | United States Marine Corps 0311 |
+| 2017-current  | Fire Alarm Technician           |
